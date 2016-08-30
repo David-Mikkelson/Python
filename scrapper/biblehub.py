d333dc2ca0df9f092cee64bfb6e1987b87d00263
David-Mikkelson <mikkelson_d@yahoo.com>
2016-08-29 19:04:59 -0700

    Update to database
    Genesis 2 is different still needs work

--- scrapper/biblehub.py
+++ scrapper/biblehub.py
@@ -1,6 +1,9 @@
 
 from bs4 import BeautifulSoup
 import os, re
+import sqlite3
+
+
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
@@ -8,6 +11,8 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
+conn = sqlite3.connect('tutorial.db')
+c = conn.cursor()
 
 #--------------------------------
 # program is a webscraper for Bible verses from the biblehub.com
@@ -27,43 +32,58 @@ def getPage(name):
 	return page
 	
 
-def extractVerses(page, book, chapter, FILE):	
+def extractVerses(page, book, chapter, version):	
+	
+
 	#----------------------
 	# 1. Using BeautifulSoup tak page and remove foot Notes and Section Heads from page
 	soup = BeautifulSoup(page, "html.parser")
-	# removing footnotes from page
-	footnotes_tag = soup.find("span", {"class": "mainfootnotes"}).extract()
-	footnotes_tag = soup.find("span", {"class": "nivfootnote"}).extract()
-	# removing Section Heads from the content
-	junk = soup.find_all("p", {"class": "sectionhead"})
-	for i in junk:
-		junk1 = i.extract()
+	
+	try:
+		# removing footnotes from page
+		footnotes_tag = soup.find("span", {"class": "mainfootnotes"}).extract()
+		footnotes_tag = soup.find("span", {"class": "nivfootnote"}).extract()
+	except:
+		print("No footnotes in {} {}".format(book, chapter))
+	
+	try:
+		# removing Section Heads from the content
+		junk = soup.find_all("p", {"class": "sectionhead"})
+		for i in junk:
 
+			junk1 = i.extract()
+	except:
+		print("no Junk")
 	#----------------------------
 	# 2. the div that contains the Bible verses has a class of chap
 	chapter_contents = soup.find("div", {"class": "chap"})
 	#-----------
 	# 3. Adjusting content to be on different lines
 	better_contents = chapter_contents.prettify()
+	file2 = open("testing", "w")
+	print(better_contents, file = file2)
+	file2.close()
 	# 4. Removing final tags from page
 	removed_tags = re.sub("(?<=<).*(?=>)|[<>]", '', str(better_contents))
 	
-	# 5. Adding file for temp output
-	outfile = open(FILE, 'a')
-	print("{} {}".format(book, chapter))
-	print("{} {}".format(book, chapter), file = outfile)
-
-	# 6. Formating and removing white space.
+	#testing chap 2 genesis
+	file1 = open("testing1", 'w')
+	print(removed_tags, file=file1)
+	file1.close()
+	# 5. Formating and removing white space.
 	lines = removed_tags.split("\n")
 	verse_number = 0
 	verse_text = ""
 	for line in lines:
+	
 		line = re.sub("^\s+", '', line)
+		
 		try:
 			verse_number = int(line)
 			if verse_number - 1  > 0:
-				print("{} {}".format(verse_number - 1, verse_text))
-				print("{} {}".format(verse_number - 1, verse_text), file = outfile)
+				i = verse_number - 1
+				print("{} {}".format(i, verse_text))
+				#updateSQL(book, chapter, i, verse_text, version)
 				verse_text = ""
 
 		except ValueError:
@@ -73,19 +93,33 @@ def extractVerses(page, book, chapter, FILE):
 	
 	# 7. print final verse
 	print("{} {}".format(verse_number, verse_text))		
-	print("{} {}".format(verse_number - 1, verse_text), file = outfile)
-	# 8. close file
-	outfile.close()
+	#updateSQL(book, chapter, verse_number, verse_text, version)
+	
+
+def updateSQL(book, chapter, verse, text, version):
+	# 1. Check to see if table exists 
+	# 2. Check to see if verse is already in the database
+	# 3. if not insert
+	c.execute("CREATE TABLE IF NOT EXISTS bibleVerses (ID TEXT, book TEXT, chapter INTEGER, verse INTEGER, verse_text TEXT, version TEXT, UNIQUE(ID))")
+
+	#Create ID
+	ID = "{}-{}-{}".format(book, chapter, verse)
+	table_data = "'{}', '{}', {}, {}, '{}', '{}'".format(ID, book, chapter, verse, text, version)
+	table_row = "ID, book, chapter, verse, verse_text, version"
+	c.execute("INSERT OR IGNORE INTO bibleVerses ({}) VALUES ({})".format(table_row, table_data))
+	conn.commit()
 
 
 
 
-index = 1
-index_max = 5
+
+index = 2
+index_max = 2
 while index <= index_max:
+	version = "NIV"
 	book = "genesis"
 	webpage = getPage("http://biblehub.com/niv/{}/{}.htm".format(book, index))
-	extractVerses(webpage, book, index, "biblehub-{}".format(book))
+	extractVerses(webpage, book, index, version)
 	index += 1
 
 
